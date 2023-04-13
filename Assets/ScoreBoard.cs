7a83e2e798b6d8b8cf2952beca9fc2c3e77ad28b
rodrifera <rodrigonestorfera@gmail.com>
2023-04-13 18:21:14 -0300

    added score (only increments, doesnt reset), fix reset pins

---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Shows an ordered list of messages via a text mesh
+/// </summary>
+public class ScoreBoard : MonoBehaviour
+{
+    [Tooltip("The text mesh the message is output to")]
+    public TextMeshProUGUI messageOutput = null;
+
+    // What happens once the list is completed
+
+    public int score = 0;
+
+
+    private void Start()
+    {
+        ShowMessage();
+    }
+
+    public void UpdateScore()
+    {
+        ShowMessage();
+    }
+
+
+    private void ShowMessage()
+    {
+        messageOutput.text = "Score: " + score;
+    }
+
+ 
+}
--- Assets/Scripts/ResetPin.cs
+++ Assets/Scripts/ResetPin.cs
@@ -8,6 +8,9 @@ public class ResetPin : MonoBehaviour
     private Quaternion originalRotation;
     private Rigidbody rigidBody;
 
+    public ScoreBoard scoreBoard;
+    private bool knocked = false;
+
     void Start()
     {
         // Store the original position and rotation
@@ -28,6 +31,9 @@ public class ResetPin : MonoBehaviour
         // Reset the ball's velocity and angular velocity
         rigidBody.velocity = Vector3.zero;
         rigidBody.angularVelocity = Vector3.zero;
+        scoreBoard.score += 1;
+        scoreBoard.UpdateScore();
+        knocked = false;
     }
 
     // Coroutine to wait for 3 seconds before resetting the ball
@@ -36,26 +42,33 @@ public class ResetPin : MonoBehaviour
         // Wait for 3 seconds
         yield return new WaitForSeconds(3f);
 
+        
         // Reset the ball
         ResetP();
+    }
+
+    public float RotationDifference()
+    {
+        return Quaternion.Angle(gameObject.transform.rotation, originalRotation);
     }
 
     // Function to detect when the throw is finished
     void Update()
     {
-        /*// Check if the ball has fallen off the lane or reached the pins
-        if (gameObject.transform.position.z != originalPosition.z || gameObject.transform.position.x != originalPosition.x || gameObject.transform.position.y != originalPosition.y)
+        // Check if the ball has fallen off the lane or reached the pins
+        if (!knocked && RotationDifference() > 5)
         {
+            knocked = true;
             // Start the coroutine to reset the ball after a delay
             StartCoroutine(ResetPinAfterDelay());
-        }*/
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.name == "Pin" || other.gameObject.name == "Ball" || other.gameObject.tag == "border" || other.gameObject.tag == "outerfloor")
+        /*if(other.gameObject.name == "Pin" || other.gameObject.name == "Ball" || other.gameObject.tag == "border" || other.gameObject.tag == "outerfloor")
         {
             StartCoroutine(ResetPinAfterDelay());
-        }
+        }*/
     }
 }
