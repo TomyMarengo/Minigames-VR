47c0270a15e642e26c01ced343c0d3ffb02568db
rodrifera <rodrigonestorfera@gmail.com>
2023-06-22 19:41:33 -0300

    fadeout sarchophagus

--- Assets/Scripts/TriggerSarchophagus.cs
+++ Assets/Scripts/TriggerSarchophagus.cs
@@ -4,36 +4,39 @@ using UnityEngine;
 
 public class TriggerSarchophagus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Light[] lights;
-    bool hasInteracted = false;
-    private void Start()
-    {
-        // Find all lights in the scene
-        lights = FindObjectsOfType<Light>();
 
-        
-    }
+
+    public Renderer renderer;
+
+    private bool hasInteracted = false;
+    public float fadeSpeed = 1f;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (!hasInteracted && other.CompareTag("Player"))
         {
-            Debug.Log("Player has interacted with the sarchophagus");
-            StartCoroutine(turnOffLights());
+            Debug.Log("Player has interacted with the pedestal");
+            StartCoroutine(fadeOut());
             hasInteracted = true;
         }
     }
 
-    private IEnumerator turnOffLights()
+    private IEnumerator fadeOut()
     {
-        // Loop through each light and turn it off
-        foreach (Light light in lights)
+
+        while (this.renderer.material.color.a > 0)
         {
-            light.intensity = 0f;
+            Color objectColor = this.renderer.material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.renderer.material.color = objectColor;
+            Debug.Log("fading out " + this.renderer.material.color.a);
+            yield return null;
         }
-        yield return null;
+
+        
+
     }
 
     // on trigger exit to reset the pedestal at same speed as it was lifted
@@ -41,19 +44,25 @@ public class TriggerSarchophagus : MonoBehaviour
     {
         if (hasInteracted && other.CompareTag("Player"))
         {
-            Debug.Log("Player has left the sarchophagus");
-            StartCoroutine(resetLights());
+            Debug.Log("Player has left the pedestal");
+            StartCoroutine(fadeIn());
             hasInteracted = false;
         }
     }
 
-    private IEnumerator resetLights()
+    private IEnumerator fadeIn()
     {
-        // Loop through each light and turn it on
-        foreach (Light light in lights)
+
+        while (this.renderer.material.color.a < 1)
         {
-            light.intensity = 1f;
+            Color objectColor = this.renderer.material.color;
+            float fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.renderer.material.color = objectColor;
+            Debug.Log("fading in" + this.renderer.material.color.a);
+            yield return null;
         }
-        yield return null;
+
     }
 }
