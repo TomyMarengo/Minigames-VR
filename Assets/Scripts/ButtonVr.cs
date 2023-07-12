47535497c026c6c4eba20dc22eb32dc567c08529
rodrifera <rodrigonestorfera@gmail.com>
2023-07-11 21:33:50 -0300

    moved screen

--- Assets/Scripts/ButtonVr.cs
+++ Assets/Scripts/ButtonVr.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class ButtonVr : MonoBehaviour
 {
 
-    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private float threshold = 0.01f;
     [SerializeField] private float deadZone = 0.05f;
     private Keypad keypad;
     public int value;
